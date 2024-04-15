739ef5c1c25ef121667b5bd9a4cef39e6cbe3a1e
NanciuDavid <nanciualexandrudavid22@stud.ase.ro>
2024-04-16 01:36:39 +0300

    Added part10_03 & part10_04

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -8,7 +8,14 @@ public class PositiveNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        
     }
 
+    
+public static List<Integer> positive(List<Integer> numbers){
+    List<Integer> positivesValues = numbers.stream()
+                                           .filter(value -> value > 0)
+                                           .collect(Collectors.toList());
+    return positivesValues;
+}
 }
